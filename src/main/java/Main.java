f7d014532208b30a57497f26e624dc942c089a07
Michael Bendtsen <mich.bendtsen@gmail.com>
2020-12-07 11:09:54 +0100

    BongoSort With Reverse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,12 @@
 import processing.core.PApplet;
 import processing.data.IntList;
 
-public class Main extends PApplet{
+public class Main extends PApplet {
     public static void main(String[] args) {
         PApplet.main("Main");
     }
-PApplet p;
+
+    PApplet p;
 
     IntList liste = new IntList();
     IntList nyTal = new IntList();
@@ -14,86 +15,94 @@ PApplet p;
     @Override
     public void settings() {
 
-        size(500,500);
+        size(500, 500);
     }
+
     public static IntList tal(IntList tal, int k) {
-        if (tal.size()<10) {
+        if (tal.size() < 10) {
             tal.append(k);
-            k=k-1;
-            tal(tal,k);
+            k = k - 1;
+            tal(tal, k);
+            return tal;
+        } else {
             return tal;
         }
-        else{return tal;}
     }
 
-    public IntList bongoShuffle(IntList tal,int k,IntList nyTal){
-
+    public IntList bongoShuffle(IntList tal, int k, IntList nyTal) {
 
 
         int c;
-        if(0<k) {
-println("Bongo");
-            c = (int)random(0,tal.size());
+        if (0 < k) {
+            println("Bongo");
+            c = (int) random(0, tal.size());
             nyTal.append(tal.get(c));
             tal.remove(c);
             k -= 1;
-            bongoShuffle(liste, k,nyTal);
+            bongoShuffle(liste, k, nyTal);
             return nyTal;
-        }else
-return nyTal;
+        } else
+            return nyTal;
     }
 
-    public IntList bongoSwap(IntList tal,int start,int slut){
+    public IntList bongoSwap(IntList tal, int start, int slut) {
         IntList nyTal = new IntList();
         nyTal = tal;
 
         println(tal);
         int b = nyTal.get(start);
-         nyTal.set(start,tal.get(slut));
-         nyTal.set(slut,b);
+        nyTal.set(start, tal.get(slut));
+        nyTal.set(slut, b);
         println(tal);
-         return nyTal;
+        return nyTal;
 
 
     }
 
-    public void bongoSort(IntList tal,int k) {
+    public void bongoSort(IntList tal, int k, boolean reverse) {
         if (k > 1) {
+            if (reverse) {
+                if (tal.get(k - 1) < tal.get(k - 2)) {
 
+                    bongoSwap(tal, k - 1, k - 2);
+                    k = 10;
 
-            if (tal.get(k-1) > tal.get(k - 2)) {
+                } else {
+                    k -= 1;
+                    bongoSort(tal, k, true);
+                }
+            } else if (tal.get(k - 1) > tal.get(k - 2)) {
+
                 bongoSwap(tal, k - 1, k - 2);
                 k = 10;
 
-            } else{ k -= 1;
-                bongoSort(tal,k);
+            } else {
+                k -= 1;
+                bongoSort(tal, k, false);
             }
 
         }
     }
 
 
+    public void display(IntList nummer, int k) {
 
+        int num = nummer.get(k);
+        fill(200);
+        rect(width / 10 * k, height, width / 10, -num * 20);
+        fill(0);
+        text(num, width / 10 * k, height - 10);
+        if (k < nummer.size() - 1) {
+            display(nummer, ++k);
+        }
 
-
-   public void display(IntList nummer, int k) {
-
-       int num = nummer.get(k);
-       fill(200);
-       rect(width/10* k, height, width/10, -num * 20);
-       fill(0);
-       text(num,width/10*k,height-10);
-       if (k< nummer.size()-1) {
-           display(nummer, ++k);
-       }
-
-   }
+    }
 
     @Override
-    public void setup(){
-    liste =tal(liste ,10);
-   liste = bongoShuffle(liste,10,nyTal);
-    println(liste);
+    public void setup() {
+        liste = tal(liste, 10);
+        liste = bongoShuffle(liste, 10, nyTal);
+        println(liste);
 
     }
 
@@ -101,13 +110,16 @@ return nyTal;
     @Override
     public void draw() {
         clear();
-        display(liste,0);
+        display(liste, 0);
 
     }
 
     @Override
     public void mousePressed() {
-         bongoSort(liste,10);
+        if (mouseButton == LEFT)
+            bongoSort(liste, 10, false);
+        else
+            bongoSort(liste, 10, true);
     }
 }
 
